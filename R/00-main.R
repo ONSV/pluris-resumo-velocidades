7d53f69305b4a4683bade9d3653946596414ca61
Pedro Augusto Borges dos Santos <pabsantos21@gmail.com>
2024-02-19 16:50:09 -0300

    Insere os dados no IPPUC no projeto

--- R/00-main.R
+++ R/00-main.R
@@ -4,10 +4,17 @@ library(Hmisc)
 library(ggtext)
 library(glue)
 library(janitor)
+library(sf)
 
 dados_bairros <- 
   readxl::read_xlsx("data/Dados Bairros media v3.xlsx") |> 
   janitor::clean_names()
 
+# bairros_cwb <- 
+#   st_read("data/DIVISA_DE_BAIRROS.shp") |> 
+#   janitor::clean_names()
+
 source("R/01-correlacao.R")
 source("R/02-wilcox.R")
+
+
